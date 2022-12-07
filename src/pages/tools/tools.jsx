173c954378300306d2b3db42a79fea582ebc79db
Abhishek Singh <abbyswag25@gmail.com>
2022-12-07 22:18:36 +0530

    v1.0.0

--- src/pages/tools/tools.jsx
+++ src/pages/tools/tools.jsx
@@ -17,7 +17,14 @@ export default function Tools(){
                 </Typography>
                 <br/>
                 <Stack spacing={2} direction={{ xs: 'column', sm: 'row'}} width={'100%'}>
-                    <Card name="Paper" description="A autonomous text editor, suppports most of the tools." link='/tools/paper'></Card>
+                    <Card name="Paper" description="An autonomous text editor, suppports most of the tools." link='/tools/paper'></Card>
+                </Stack>
+                <br/>
+                <Typography variant="h6">
+                    In progress, Coming soon...
+                </Typography>
+                <br/>
+                <Stack spacing={2} direction={{ xs: 'column', sm: 'row'}} width={'100%'}>
                     <Card name="Paraphraser" description="For repharing large chunks of content." link='/tools/paraphraser'></Card>
                     <Card name="Summariser" description="For summarise large chunk of content." link='/tools/playground'></Card>
                     <Card name="Referencer" description="Search and generate references and citations in different format." link='/tools/playground'></Card>
